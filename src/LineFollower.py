7a71366888596ba5625480db3c641478f7747e14
freddie-mj <116028471+freddie-mj@users.noreply.github.com>
2025-02-21 14:58:01 +0000

    Create LineFollower.py
    Contains LineFollower class. This class will be responsible for detecting the position of the line and adjusting the vehicle so that it is centred on the line.

---
+++ src/LineFollower.py
@@ -0,0 +1,24 @@
+from TrackerSensor import TrackerSensor
+from Motor import Motor
+
+class LineFollower():
+    def __init__(self, tracker_sensors: list[TrackerSensor], left_motor: Motor, right_motor: Motor):
+        # Tracker sensors should be ordered from left to right
+        self.__tracker_sensors = tracker_sensors
+        self.__sensor_count = len(tracker_sensors)
+        self.__left_motor = left_motor
+        self.__right_motor = right_motor
+
+    def get_line_pos(self) -> float | None:
+        # Tracker sensor readings are either 0 or 1. Assuming tracker sensors are evenly spaced (with unit distance),
+        # we can estimate the position of the line as the 'center of mass' of the readings.
+        # 
+        # The position will (hopefully) be a multiple of a half (if the thresholds are right), as 
+        # a maximum of two sensors should read high.
+        readings = [ts.read() for ts in self.__tracker_sensors]
+        success = readings.count(True)
+        if success:
+            pos = sum([(i - 1.5) * readings[i] for i in range(self.__sensor_count)]) / success
+            return pos
+        return None
+
