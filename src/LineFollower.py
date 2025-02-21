f2d6f28e873e6a2d7f50bfd9fd46499ba696fb39
freddie-mj <116028471+freddie-mj@users.noreply.github.com>
2025-02-21 19:41:02 +0000

    Added framework for PID control to LineFollower
    Values of K_P, K_D and K_I need to be determined empirically

--- src/LineFollower.py
+++ src/LineFollower.py
@@ -2,10 +2,16 @@ from TrackerSensor import TrackerSensor
 from Motor import Motor
 
 class LineFollower():
+    K_P = 2.5
+    K_D = 0.02
+    K_I = 12
+
     def __init__(self, tracker_sensors: list[TrackerSensor]):
         # Tracker sensors should be ordered from left to right
         self.__tracker_sensors = tracker_sensors
         self.__sensor_count = len(tracker_sensors)
+        self.__last_error = self.get_line_pos()
+        self.__integral_error = 0
 
     def get_line_pos(self) -> float | None:
         # Tracker sensor readings are either 0 or 1. Assuming tracker sensors are evenly spaced (with unit distance),
@@ -20,3 +26,12 @@ class LineFollower():
             return pos
         return None
 
+    def get_power_error(self) -> float:
+        # Returns how much extra power (+ve or -ve) needs to be delivered to right wheel than left to correct angle
+        abs_error = self.get_line_pos()
+        power_error = abs_error * LineFollower.K_P
+        derivative_error = abs_error - self.__last_error
+        power_error += derivative_error * LineFollower.K_D
+        self.__integral_error += abs_error
+        power_error += self.__integral_error * LineFollower.K_I
+        return power_error
