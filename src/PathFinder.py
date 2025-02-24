3b15cfacb187ff0f857514b599c80612691f2f27
cxtraa <moosasaghir10@gmail.com>
2025-02-24 20:40:22 +0000

    Completed the dummy robot, it can fully pathfind on the graph

--- src/PathFinder.py
+++ src/PathFinder.py
@@ -1,39 +1,60 @@
 
-graph = {
-    (0,0) : [(103, 0), (-34, 0), (0, -29)],
-    (0, -29) : [(0, 0)],
-    (103, 0) : [(103, -31), (103, 88), (0,0)],
-    (-34, 0) : [(-34, 32), (-104, 0), (0, 0)],
-    (-104, 0) : [(-104, -31), (-104, 88), (-34, 0)],
-    (-104, 88) : [(-104, 162), (-1, 88), (-104, 0)],
-    (-34, 32) : [(-34, 0)],
-    (-1, 88) : [(32, 88), (32, 66), (-104, 88)],
-    (32, 88) : [(-1, 88), (32, 66), (103, 88)],
-    (103, 88) : [(103, 162), (103, 0), (32, 88)],
-    (103, 162) : [(103, 88), (40, 162)],
-    (40, 162) : [(103, 162), (-1, 162), (40, 139)],
-    (40, 139) : [(40, 162)],
-    (-1, 162) : [(40, 162), (-1, 123), (-104, 162)],
-    (-104, 162) : [(-104, 88), (-1, 162)],
-    (-27, 123) : [(-1, 123)],
-    (-1, 123) : [(-27, 123), (-1, 162), (-1, 88)],
-    (32, 66) : [(32, 88)],
-    (-104, 31) : [(-104, 0)],
-    (103, -31) : [(103, 0)], 
-}
+from config import GRAPH
 
-
+import heapq
 
 class PathFinder:
-    # Initialiser takes in the graph
     def __init__(self, graph):
         self.graph = graph
+    
+    def find_shortest_path(self, node_A, node_B):
+        """
+        Given nodes A, B, find the shortest path between them, using the lazy version of Dijkstra.
+        """
+        N = len(self.graph)
+        pq = [(0, node_A)]
+        best = {node : float('inf') for node in self.graph.keys()}
+        prev = {node : None for node in self.graph.keys()}
+        visited = set()
+        best[node_A] = 0
+        
+        while pq:
+            curr_dist, curr_node = heapq.heappop(pq)
+            if best[curr_node] < curr_dist:
+                continue
+            if curr_node in visited:
+                continue
+            visited.add(curr_node)
+            for child_node in self.graph[curr_node]:
+                if child_node not in visited:
+                    curr_x, curr_y = curr_node
+                    next_x, next_y = child_node
+                    edge_dist = abs(next_x - curr_x) + abs(next_y - curr_y)
+                    new_dist = curr_dist + edge_dist
+                    if new_dist < best[child_node]:
+                        best[child_node] = new_dist
+                        heapq.heappush(pq, (new_dist, child_node))
+                        prev[child_node] = curr_node
+        return self.reconstruct_path(prev, node_A, node_B)    
+
+    def reconstruct_path(self, prev, start, end):
+        """
+        From prev (produced by Dijkstra) reconstruct the path.
+        We do this by going in reverse order then reversing the array.
+        """
+        path = []
+        curr_node = end
+        while curr_node != start:
+            path.append(curr_node)
+            curr_node = prev[curr_node]
+        path.append(start)
+        path.reverse()
+        return path
+
 
 def main():
-    sum_of_degrees = 0
-    for key, value in graph.items():
-        sum_of_degrees += len(value)
-    print(sum_of_degrees)
+    pathFinder = PathFinder(GRAPH)
+    print(pathFinder.find_shortest_path((0,0), (103, -31)))
 
 if __name__ == "__main__":
     main()
---
+++ src/Robot.py
@@ -0,0 +1,86 @@
+from config import GRAPH
+from PathFinder import PathFinder
+
+class Robot:
+    def __init__(self, graph, start_node=(0,0), start_dir=0):
+        """
+        The possible robot directions are:
+            - 0 North
+            - 1 East
+            - 2 South
+            - 3 West
+        """
+        self.dir = start_dir
+        self.curr_node = start_node
+        self.graph = graph
+        self.path_finder = PathFinder(graph=graph)
+    
+    def navigate(self, dest):
+        """
+        Move the robot to `dest` where multiple nodes might be in between.
+        """
+        shortest_path = self.path_finder.find_shortest_path(self.curr_node, dest)
+        for i in range(1, len(shortest_path)):
+            self.move(shortest_path[i])
+
+    def forward(self) -> None:
+        c_x, c_y = self.curr_node
+        for neighbor in self.graph[self.curr_node]:
+            n_x, n_y = neighbor
+            cond1 = self.dir == 0 and n_y > c_y
+            cond2 = self.dir == 1 and n_x > c_x
+            cond3 = self.dir == 2 and n_y < c_y
+            cond4 = self.dir == 3 and n_x < c_x
+            if cond1 or cond2 or cond3 or cond4:
+                self.curr_node = neighbor
+                break            
+
+    def turn_right(self):
+        self.dir = (1 + self.dir) % 4
+
+    def turn_left(self):
+        self.dir = (-1 + self.dir) % 4
+    
+    def change_dir(self, desired_dir):
+        """
+        Find the optimal turns to change the direction.
+        E.g. if going from North to East, we should call turn_right() once.
+        """
+
+        if desired_dir == (self.dir + 1) % 4:
+            self.turn_right()
+        elif desired_dir == (self.dir - 1) % 4:
+            self.turn_left()
+        elif desired_dir == (self.dir + 2) % 4:
+            self.turn_right()
+            self.turn_right()
+    
+    def move(self, dest):
+        """
+        Move the robot from the current node to `dest`, where current node and dest are NEIGHBORS.
+        """
+        x_1, y_1 = self.curr_node
+        x_2, y_2 = dest
+
+        if x_2 > x_1:
+            self.change_dir(1)
+        elif x_2 < x_1:
+            self.change_dir(3)
+        elif y_2 > y_1:
+            self.change_dir(0)
+        elif y_2 < y_1:
+            self.change_dir(2)
+        
+        self.forward()
+
+    def __str__(self) -> str:
+        directions = ["North", "East", "South", "West"]
+        return f"Position: {self.curr_node} | Direction : {directions[self.dir]}"
+
+def main():
+    robot = Robot(graph=GRAPH, start_node=(103,0), start_dir=1)
+    robot.navigate((-104, 162))
+    print(robot)
+
+if __name__ == "__main__":
+    main()
