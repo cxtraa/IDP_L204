10e2918bd0b2ec695c650feff20d266740e2bb29
freddie-mj <116028471+freddie-mj@users.noreply.github.com>
2025-02-27 09:30:53 +0000

    Renamed Motor.m1Dir and Motor.pwm1 to Motor.dir and Motor.pwm

--- src/Motor.py
+++ src/Motor.py
@@ -8,24 +8,24 @@ class Motor:
         # Used for assigning the right motor number to the PWM and DIR
         motor_numbers = [(0, 1), (3, 2), (4, 5), (7, 6)]
         dir_pin, pwm_pin = motor_numbers[motor_number - 1]
-        self.m1Dir = Pin(dir_pin, Pin.OUT)
-        self.pwm1 = PWM(Pin(pwm_pin))
-        self.pwm1.freq(1000)
-        self.pwm1.duty_u16(0)
+        self.dir = Pin(dir_pin, Pin.OUT)
+        self.pwm = PWM(Pin(pwm_pin))
+        self.pwm.freq(1000)
+        self.pwm.duty_u16(0)
     
     def off(self) -> None:
-        self.pwm1.duty_u16(0)
+        self.pwm.duty_u16(0)
     
     def forward(self, speed : float) -> None:
-        self.m1Dir.value(0)
+        self.dir.value(0)
         self.set_duty_cycle(speed)
     
     def set_duty_cycle(self, percentage : float) -> int:
         if not 0 <= percentage <= 100:
             raise ValueError("Motor goes between 0 to 100")
         duty_cycle = int((2**16 - 1) * percentage) # the board expects a 16 bit unsigned integer from 0 - (2^16-1) inclusive
-        self.pwm1.duty_u16(duty_cycle)
+        self.pwm.duty_u16(duty_cycle)
 
     def reverse(self, speed : float) -> None:
-        self.m1Dir.value(1)
+        self.dir.value(1)
         self.set_duty_cycle(speed)
