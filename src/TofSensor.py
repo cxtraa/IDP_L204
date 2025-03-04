d7e819b8afe0ba119d68b7f651404ca9430b7fef
freddie-mj <116028471+freddie-mj@users.noreply.github.com>
2025-03-04 09:31:32 +0000

    Bug fix

--- src/TofSensor.py
+++ src/TofSensor.py
@@ -3,7 +3,7 @@ from vl53l0x import VL53L0X
 
 class TofSensor():
     def __init__(self, sda_pin: int, scl_pin: int):
-        i2c_bus = I2C(0, sda=Pin(sda_pin), scl=Pin(scl_pin))
+        i2c_bus = I2C(1, sda=Pin(sda_pin), scl=Pin(scl_pin))
         self.__vl = VL53L0X(i2c_bus)
         self.__vl.set_measurement_timing_budget(40000)
         self.__vl.set_Vcsel_pulse_period(self.__vl.vcsel_period_type[0], 12)
